792aede6f258d5960e59413a4bcf607d4a2c95ad
Chandler King <ckmotox356@gmail.com>
2019-11-13 16:53:16 -0700

    Added the form button field and the onSubmit event handler.

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -4,10 +4,16 @@ import SignInForm from './signinForm';
 
 
 class SignIn extends Component {
+
+  onSubmit = (field) => {
+      field.preventDefault();
+      console.log(field);
+  }
+
   render() {
     return(
         <div className="sign-in">
-            <SignInForm className='sign-in__form' />
+            <SignInForm handleSubmit={this.onSubmit} className='sign-in__form' />
         </div>
     )
 }
--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import { reduxForm, Field} from 'redux-form';
 
-import { FormInput } from '../formFields';
+import { FormInput, FormButton } from '../formFields';
 
 class SignInForm extends Component {
   render() {
-    const { className } = this.props;  
+    const { className, handleSubmit} = this.props;  
     return(
-        <form className={`${className} sign-in-form`}>
+        <form className={`${className} sign-in-form`} onSubmit={handleSubmit}>
             <Field className="sign-in-form__email" name="email" type="email" title="Email" placeholder="Email" component={FormInput} />
             <Field className="sign-in-form__password" name="password" type="password" title="Password" placeholder="Password" component={FormInput} />
+            <Field className="sign-in-form__login" name="login" type="submit" title="Login" component={FormButton} onClick={() => console.log("submitting")} />
         </form>
     )
 }
