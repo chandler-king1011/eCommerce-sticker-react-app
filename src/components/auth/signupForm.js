fcad7d939628b0dd8ca82a137ad3d34e822e6071
Chandler King <ckmotox356@gmail.com>
2019-11-18 19:14:25 -0700

    Finished styling the sign-up component. Also added the ability to push back to the login form from the sign in form using the back button.

--- src/components/auth/signupForm.js
+++ src/components/auth/signupForm.js
@@ -26,13 +26,13 @@ class SignUpForm extends Component {
             <Field className="sign-up-form__name" name="name" type="name" title="Name" placeholder="Name" component={FormInput} />
             <Field className="sign-up-form__email" name="email" type="email" title="Email" placeholder="Email" component={FormInput} />
             <Field className="sign-up-form__password" name="password" type="password" title="Password" placeholder="Password" component={FormInput} />
-            <Field className="sign-up-form__confirm-password" name="confirm" type="password" title="Confirm Password" placeholder="Confirm Password" component={FormInput} />
-
-
+            <Field className="sign-up-form__confirm" name="confirm" type="password" title="Confirm Password" placeholder="Confirm Password" component={FormInput} />
             <div className="sign-up-form__border"></div>
-            <Field className="sign-up-form__back" name="back" type="button" title="Back" component={FormButton} onClick={() => console.log("go back")} gray={true} />
-            <Field className="sign-up-form__login" name="login" type="submit" title="Login" component={FormButton} onClick={() => console.log("submitting")} />
-            <Details className="sign-up-form__details" title="Password requirements" links={links}/>
+            <div className="sign-up-form__buttons">
+                <Field className="sign-up-form__back" name="back" type="button" title="Back" component={FormButton} onClick={() => history.push('/signin')} gray={true} />
+                <Field className="sign-up-form__create" name="create" type="submit" title="Create Account" component={FormButton} onClick={() => history.push('./account')} />
+            </div>
+            <Details className="sign-up-form__details" title="Password requirements" links={links} makeLinks={false}/>
         </form>
     )
 }
