f754472d9d11c6f1feeb99c4cf41953743846cb3
Chandler King <ckmotox356@gmail.com>
2019-12-05 18:46:18 -0700

    Built a card flip animation ability for the shop product component.

--- src/components/shop/shopProduct.js
+++ src/components/shop/shopProduct.js
@@ -8,19 +8,26 @@ class ShopProduct extends Component {
         const { title, description, price } = this.props;
         return (
             <div className="shop-product">
-                <div className="shop-product__top">
-                    <div className="shop-product__title">{title}</div>
-                    <div className="shop-product__description">{description}</div>
-                </div>
-                <div className="shop-product__bottom">
-                    <div className="shop-product__price-incrementor-wrapper">
-                        <GreenPriceTag className="shop-product__price" price={price} />
-                        <Quantity quantity={1} className="shop-product__quantity" />
+              <div className="shop-product__front">
+                  <img className="shop-product__front__image" src="http://via.placeholder.com/220x220" />
+                  <div className='shop-product__front__title'>{title}</div>
+
+              </div>  
+              <div className="shop-product__back">
+                <div className="shop-product__back__top">
+                        <div className="shop-product__back__title">{title}</div>
+                        <div className="shop-product__back__description">{description}</div>
                     </div>
-                    <div className="shop-product__button-wrapper">
-                        <div className="shop-product__add-to-cart">Add To Cart</div>
-                    </div>0
-                </div>1
+                    <div className="shop-product__back__bottom">
+                        <div className="shop-product__back__price-incrementor-wrapper">
+                            <GreenPriceTag className="shop-product__back__price" price={price} />
+                            <Quantity quantity={1} className="shop-product__back__quantity" />
+                        </div>
+                        <div className="shop-product__back__button-wrapper">
+                            <div className="shop-product__back__add-to-cart">Add To Cart</div>
+                        </div>
+                    </div>
+              </div>
             </div>
         )
     }
