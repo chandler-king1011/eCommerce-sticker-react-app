b1ac9cab27bfda8d4ff71785c94cfa0695de7b16
Chandler King <ckmotox356@gmail.com>
2019-12-05 18:14:46 -0700

    Styled the shop page and shop product component

--- src/components/shop/shopProduct.js
+++ src/components/shop/shopProduct.js
@@ -8,11 +8,19 @@ class ShopProduct extends Component {
         const { title, description, price } = this.props;
         return (
             <div className="shop-product">
-                <div className="shop-product__title">{title}</div>
-                <div className="shop-product__description">{description}</div>
-                <GreenPriceTag className="shop-product__price" price={price} />
-                <Quantity quantity={1} className="shop-product__quantity" />
-                <div className="shop-product__add-to-cart">Add To Cart</div>
+                <div className="shop-product__top">
+                    <div className="shop-product__title">{title}</div>
+                    <div className="shop-product__description">{description}</div>
+                </div>
+                <div className="shop-product__bottom">
+                    <div className="shop-product__price-incrementor-wrapper">
+                        <GreenPriceTag className="shop-product__price" price={price} />
+                        <Quantity quantity={1} className="shop-product__quantity" />
+                    </div>
+                    <div className="shop-product__button-wrapper">
+                        <div className="shop-product__add-to-cart">Add To Cart</div>
+                    </div>0
+                </div>1
             </div>
         )
     }
